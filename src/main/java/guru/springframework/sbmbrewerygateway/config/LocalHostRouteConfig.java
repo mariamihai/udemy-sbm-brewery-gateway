ecd219c797184416193cdcc933092f3c5cab80fe
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-24 01:00:55 +0200

    Assignment 16 - Configure Beer Order Service. Closes #4

--- src/main/java/guru/springframework/sbmbrewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/springframework/sbmbrewerygateway/config/LocalHostRouteConfig.java
@@ -12,9 +12,14 @@ public class LocalHostRouteConfig {
     public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-                    .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beer/beerUpc/*")
+                    .route(r -> r.path("/api/v1/beer*",
+                                       "/api/v1/beer/*",
+                                       "/api/v1/beer/beerUpc/*")
                                  .uri("http://localhost:8080")
                                  .id("beer-service"))
+                    .route(r -> r.path("/api/v1/customers/**")
+                                 .uri("http://localhost:8081")
+                                 .id("beer-order-service"))
                 .build();
     }
 }
