778e1c52d97d523e6543ad825dcbee398d12c2be
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-24 01:36:57 +0200

    Add route for Inventory Service. Closes #5

--- src/main/java/guru/springframework/sbmbrewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/springframework/sbmbrewerygateway/config/LocalHostRouteConfig.java
@@ -20,6 +20,10 @@ public class LocalHostRouteConfig {
                     .route(r -> r.path("/api/v1/customers/**")
                                  .uri("http://localhost:8081")
                                  .id("beer-order-service"))
+                    .route(r -> r.path("/api/v1/beer/*/inventory/*",
+                                       "/api/v1/beer/*/inventory*")
+                                 .uri("http://localhost:8082")
+                                 .id("inventory-service"))
                 .build();
     }
 }
