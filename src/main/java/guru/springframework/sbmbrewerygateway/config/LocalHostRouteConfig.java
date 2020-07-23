a1e6f0bae995c9c4c62ea1c9b79bf83a02596a7c
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-23 21:45:13 +0200

    Add localhost route for Beer Service. Closes #3

---
+++ src/main/java/guru/springframework/sbmbrewerygateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.sbmbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                    .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beer/beerUpc/*")
+                                 .uri("http://localhost:8080")
+                                 .id("beer-service"))
+                .build();
+    }
+}
